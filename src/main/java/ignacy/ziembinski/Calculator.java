23229a4c670d58fc35c85306ff997fd252409d86
ignas <ignasziembinski@gmail.com>
2023-01-13 18:16:01 +0100

    9.Math

---
+++ src/main/java/ignacy/ziembinski/Calculator.java
@@ -0,0 +1,17 @@
+package ignacy.ziembinski;
+
+public class Calculator {
+
+
+
+     static int AbsoluteNumber(int n)  {
+                return Math.abs(n);
+
+        }
+
+        public static void main(String[] args) {
+        int wynik = AbsoluteNumber(-20);
+                System.out.println(wynik);
+
+        }
+}
