7618c018a9f3e1416e1ebcca9451d3bfc8d02941
ignas <ignasziembinski@gmail.com>
2023-02-03 18:11:16 +0100

    10.i dont know

--- src/main/java/ignacy/ziembinski/quizapp.java
+++ src/main/java/ignacy/ziembinski/quizapp.java
@@ -15,20 +15,44 @@ public class quizapp {
         System.out.println("Wpisz swoją odpowiedź");
         String answer = scanner.nextLine();
 
-        while (!answer.equalsIgnoreCase("c")
-                ||!answer.equalsIgnoreCase("c")){
-            System.out.println("Możliwe odpowiedzi to A B lub C");
-        answer = scanner.nextLine();
 
-        }
 
-        System.out.println("Odpowiedziałeś " + answer);
+            System.out.println("W którym roku powstał pierwsza gra Super Smash Bros");
+            System.out.println("A)1999");
+            System.out.println("B)2001");
+            System.out.println("C)1998");
 
+            System.out.println("Wpisz swoją odpowiedź");
+             answer = scanner.nextLine();
+
+
+             answer = chekanswer(scanner, answer);
+
+            System.out.println("Odpowiedziałeś " + answer);
+
+        chekanswer(answer);
+    }
+
+    private static void chekanswer(String answer) {
         if (answer.equalsIgnoreCase("c")) {
             System.out.println("Dobrze!");
         } else {
             System.out.println("Złe!");
 
         }
-        }
     }
+
+
+    private static String chekanswer(Scanner scanner, String answer) {
+        while (!answer.equalsIgnoreCase("a")
+                &&!answer.equalsIgnoreCase("b")
+                &&!answer.equalsIgnoreCase("c")){
+
+
+
+        System.out.println("Możliwe odpowiedzi to A B lub C");
+    answer = scanner.nextLine();
+
+    }
+        return answer;
+    }}
